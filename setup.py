0725b932b13adda52caffbf84cb6a306e4a7f5cb
Paul Demorest <pdemores@nrao.edu>
2018-10-09 09:10:28 -0600

    setup.py: add fitsio requirement

--- setup.py
+++ setup.py
@@ -1,2 +1,3 @@
 from distutils.core import setup
-setup(name='pypsrfits', version='0.0', py_modules=['pypsrfits'])
+setup(name='pypsrfits', version='0.0', py_modules=['pypsrfits'], 
+        install_requires=['fitsio',])
